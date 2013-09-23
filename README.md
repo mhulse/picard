0e4319dc31cbbbd3d205a96ebc5df2d00b2bd5bc
Micky Hulse <mickyhulse@gmail.com>
2013-09-22 19:15:14 -0700

    Mucho changitos.

--- README.md
+++ README.md
@@ -2,10 +2,4 @@
 
 [![ScreenShot](picard.gif)](http://www.youtube.com/watch?v=-ZxHAZChcYU)
 
-**Captain's Log, Stardate <kbd>41235.25</kbd>.**  
-Our location, planet Git Hub. Source of a rare repository needed on Federation website [mhulse.com](http://mhulse.com).  
-Starfleet has instructed me to engage in a friendly visit and open treaty negotiations to acquire this digital substance.
-
----
-
 Copyright &copy; 2013 [Micky Hulse](http://mhulse.com)
--- source/Gruntfile.js
+++ source/Gruntfile.js
@@ -7,7 +7,34 @@ module.exports = function(grunt) {
 	
 	grunt.initConfig({
 		
+		/*----------------------------------( PACKAGE )----------------------------------*/
+		
+		/**
+		 * The `package.json` file belongs in the root directory of your project,
+		 * next to the `Gruntfile`, and should be committed with your project
+		 * source. Running `npm install` in the same folder as a `package.json`
+		 * file will install the correct version of each dependency listed therein.
+		 *
+		 * Install project dependencies with `npm install` (or `npm update`).
+		 *
+		 * @see http://gruntjs.com/getting-started#package.json
+		 * @see https://npmjs.org/doc/json.html
+		 * @see http://package.json.nodejitsu.com/
+		 * @see http://stackoverflow.com/a/10065754/922323
+		 */
+		
 		pkg : grunt.file.readJSON('package.json'),
+		
+		/*----------------------------------( BOWER )----------------------------------*/
+		
+		/**
+		 * Install Bower packages. Smartly.
+		 *
+		 * Use this task to update dependencies defined in `bower.json`.
+		 *
+		 * @see https://github.com/yatskevich/grunt-bower-task
+		 * @see http://bower.io/
+		 */
 		
 		bower : {
 			
@@ -15,10 +42,10 @@ module.exports = function(grunt) {
 				
 				options : {
 					
-					targetDir : './files/plugins',
-					cleanup : true,
-					layout : 'byComponent',
-					verbose : true,
+					targetDir : './files/plugins', // A directory where you want to keep your Bower packages.
+					cleanup : true,                // Will clean target and bower directories.
+					layout : 'byComponent',        // Folder structure type.
+					verbose : true,                // Debug output.
 					
 				},
 				
@@ -26,63 +53,163 @@ module.exports = function(grunt) {
 			
 		},
 		
+		/*----------------------------------( WATCH )----------------------------------*/
+		
+		/**
+		 * Run predefined tasks whenever watched file patterns are added, changed
+		 * or deleted.
+		 *
+		 * @see https://github.com/gruntjs/grunt-contrib-watch
+		 */
+		
+		watch : {
+			
+			files : [
+				
+				'<%= jshint.init %>',
+				'./files/styles/**/*',
+				'./files/templates/**/*',
+				
+			],
+			
+			tasks : ['default'],
+			
+		},
+		
+		/*----------------------------------( JSHINT )----------------------------------*/
+		
+		/**
+		 * Validate files with JSHint.
+		 *
+		 * @see https://github.com/gruntjs/grunt-contrib-jshint
+		 * @see http://www.jshint.com/docs/
+		 */
+		
 		jshint : {
 			
 			options : {
 				
-				jshintrc : '.jshintrc',
+				jshintrc : '.jshintrc', // Defined options and globals.
 				
 			},
 			
 			init : [
 				
 				'./Gruntfile.js',
-				'./files/scripts/**/*',
 				
 			],
 			
 		},
 		
-		sass : {
-			
-			options : {
-				
-				precision : 14,
-				noCache: true,
-				trace: true,
-				
-			},
+		/*----------------------------------( ENV )----------------------------------*/
+		
+		/**
+		 * Grunt task to automate environment configuration for future tasks.
+		 *
+		 * @see https://github.com/onehealth/grunt-env
+		 */
+		
+		env : {
 			
 			dev : {
 				
-				options : {
-					
-					style : 'expanded',
-					
-				},
-				
-				files : {
-					
-					'./temp/<%= pkg.name %>.css' : './files/styles/rex.scss',
-					'../demo/demo.css' : './files/styles/demo.scss',
-					
-				},
+				NODE_ENV : 'DEVELOPMENT',
 				
 			},
 			
 			prod : {
 				
-				options : {
-					
-					style : 'compressed',
-					
-				},
+				NODE_ENV : 'PRODUCTION',
+				
+			},
+			
+		},
+		
+		/*----------------------------------( CLEAN )----------------------------------*/
+		
+		/**
+		 * Clean files and folders.
+		 *
+		 * @see https://github.com/gruntjs/grunt-contrib-clean
+		 */
+		
+		clean : {
+			
+			options : {
+				
+				force : true, // Allows for deletion of folders outside current working dir (CWD). Use with caution.
+				
+			},
+			
+			dev : [
+				
+				'../dev/**/*',
+				
+			],
+			
+		},
+		
+		/*----------------------------------( SASS )----------------------------------*/
+		
+		/**
+		 * Compile Sass to CSS.
+		 *
+		 * @see https://github.com/gruntjs/grunt-contrib-sass
+		 * @see http://sass-lang.com/docs/yardoc/file.SASS_REFERENCE.html#output_style
+		 */
+		
+		sass : {
+			
+			options : {
+				
+				noCache: true,      // Don't cache to sassc files.
+				precision : 14,     // How many digits of precision to use when outputting decimal numbers.
+				style : 'expanded', // Output style. Can be nested, compact, compressed, expanded.
+				trace: true,        // Show a full traceback on error.
+				
+			},
+			
+			dev : {
 				
 				files : {
 					
-					'./temp/<%= pkg.name %>.min.css' : './files/styles/rex.scss',
+					'../dev/<%= pkg.name %>.css' : './files/styles/<%= pkg.name %>.scss',
 					
 				},
+				
+			},
+			
+		},
+		
+		/*----------------------------------( PREPROCESS )----------------------------------*/
+		
+		/**
+		 * Grunt task around preprocess npm module.
+		 *
+		 * @see https://github.com/onehealth/grunt-preprocess
+		 * @see https://github.com/onehealth/preprocess
+		 */
+		
+		preprocess : {
+			
+			options : {
+				
+				context : {
+					
+					title : '<%= pkg.title %>',
+					description : '<%= pkg.description %>',
+					name : '<%= pkg.name %>',
+					version : '<%= pkg.version %>',
+					homepage : '<%= pkg.homepage %>',
+					
+				},
+				
+			},
+			
+			dev : {
+				
+				src : './files/templates/index.html',
+				dest : '../dev/index.html',
 				
 			},
 			
@@ -90,12 +217,37 @@ module.exports = function(grunt) {
 		
 	});
 	
+	/*----------------------------------( TASKS )----------------------------------*/
+	
 	grunt.loadNpmTasks('grunt-bower-task');
+	
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	
+	grunt.loadNpmTasks('grunt-contrib-clean');
 	
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	
-	grunt.registerTask('default', ['jshint', 'sass:prod',]);
+	grunt.loadNpmTasks('grunt-env');
+	
+	grunt.loadNpmTasks('grunt-preprocess');
+	
+	//----------------------------------
+	
+	/**
+	 * @see https://github.com/onehealth/grunt-preprocess/issues/7
+	 * @see https://github.com/onehealth/grunt-env/issues/4
+	 */
+	
+	grunt.registerTask('printenv', function () { console.log(process.env); });
+	
+	//----------------------------------
+	
+	grunt.registerTask('init', ['jshint',]);
+	
+	grunt.registerTask('dev', ['env:dev', 'clean:dev', 'sass:dev', 'preprocess:dev',]);
+	
+	grunt.registerTask('default', ['init', 'dev',]);
 	
 };
