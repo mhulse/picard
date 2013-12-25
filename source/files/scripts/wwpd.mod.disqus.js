9acd844cde1b66f50c31346775eab01fd0ce5867
Micky Hulse <mickyhulse@gmail.com>
2013-12-25 00:53:15 -0800

    Tons o' shit!

---
+++ source/files/scripts/wwpd.mod.disqus.js
@@ -0,0 +1,59 @@
+/*jshint -W062, expr:true */
+
+
+// Load Disqus comments on click.
+// HTML: 
+WWPD.register(function() {
+	
+	'use strict';
+	
+	var $disqus = $('#disqus'),
+	    $disqus_thread;
+	
+	if ($disqus.length) {
+		
+		$disqus.hide();
+		
+		$disqus_thread = $disqus.find('#disqus_thread');
+		
+		if ($disqus_thread.length) {
+			
+			$('.disqus').click(function(e) {
+				
+				var data = $disqus_thread.data('disqus'),
+				    i;
+				
+				e.preventDefault();
+				
+				for (i in data) {
+					
+					if (data.hasOwnProperty(i)) {
+						
+						window['disqus_' + i.toLowerCase()] = data[i];
+						
+					}
+					
+				}
+				
+				$(this).fadeOut(500, function() {
+					
+					$disqus.fadeIn(1000, function() {
+						
+						$.ajax({
+							type: 'GET',
+							url: 'http://' + window.disqus_shortname + '.disqus.com/embed.js',
+							dataType: 'script',
+							cache: true
+						});
+						
+					});
+					
+				});
+				
+			});
+			
+		}
+		
+	}
+	
+}); // WWPD?
